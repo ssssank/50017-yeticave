3c4f8fc2e996440ef801a9b98d7eaa7a81a0c155
Alex Litvinov <ssssank@gmail.com>
2017-04-25 09:18:09 +0300

    Исправил отступы

--- index.php
+++ index.php
@@ -131,24 +131,24 @@ $goods = [
         </div>
         <ul class="lots__list">
             <?php foreach ($goods as $key => $val) : ?>
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="<?=$val['image']; ?>.jpg" width="350" height="260" alt="Сноуборд">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category"><?=$val['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href=""><?=$val['name']; ?></a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$val['price']; ?><b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
-                            <?=$lot_time_remaining;?>
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?=$val['image']; ?>.jpg" width="350" height="260" alt="Сноуборд">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?=$val['category']; ?></span>
+                        <h3 class="lot__title"><a class="text-link" href=""><?=$val['name']; ?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?=$val['price']; ?><b class="rub">р</b></span>
+                            </div>
+                            <div class="lot__timer timer">
+                                <?=$lot_time_remaining;?>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </li>
+                </li>
             <?php endforeach; ?>
         </ul>
     </section>
