d64139136da4f67ff31239362941c50b0cd51bee
Alex Litvinov <ssssank@gmail.com>
2017-05-01 22:51:22 +0300

    Исправил логику отображения ошибки 404

--- lot.php
+++ lot.php
@@ -2,6 +2,13 @@
 
 require_once 'functions.php';
 require_once 'alldata.php';
+$page404 = false;
+if (!isset($goods[$_GET['id']])) {
+    $page404 = true;
+    header("HTTP/1.1 404 Not Found");
+
+} else {
+}
 
 ?>
 
@@ -9,23 +16,23 @@ require_once 'alldata.php';
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title><?=$goods[$_GET['id']]['name'] ; ?></title>
+    <title><?php (($page404)) ? print ('404') : $goods[$_GET['id']]['name']; ?></title>
     <link href="css/normalize.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
 
+<?=makeTemplate('templates/header.php', []); ?>
+
 <?php
-print makeTemplate('templates/header.php', []);
-if (!isset($goods[$_GET['id']])) {
-    header("HTTP/1.1 404 Not Found");
-    print '<h1>Ошибка 404</h1>';
-    print '<h2>Лот не найден!</h2>';
-} else {
+if (!($page404)) {
     print makeTemplate('templates/main-lot.php', ['bets' => $bets, 'good' => $goods[$_GET['id']]]);
+} else {
+    print makeTemplate('templates/page404.php', []);
 }
-print makeTemplate('templates/footer.php', []);
 ?>
+
+<?=makeTemplate('templates/footer.php', []); ?>
 
 </body>
 </html>
